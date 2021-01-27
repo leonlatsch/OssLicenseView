f4be4743b7a889a32104ccfe32be6c59674fde96
Leon Latsch <dev.leonlatsch@gmail.com>
2021-01-28 00:21:16 +0100

    remove unit tests :)

--- oss-license-view/build.gradle.kts
+++ oss-license-view/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdkVersion(16)
         targetSdkVersion(30)
         versionCode = 1
-        versionName = "1.0.0-snapshot"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -57,7 +57,4 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
-    testImplementation("junit:junit:4.+")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
