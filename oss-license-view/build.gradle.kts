fa2f775634ff5a7a3c723633fa48a9b98421d98f
Leon Latsch <dev.leonlatsch@gmail.com>
2021-01-28 00:54:50 +0100

    create release number for jitpack

--- oss-license-view/build.gradle.kts
+++ oss-license-view/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdkVersion(16)
         targetSdkVersion(30)
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.1.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
